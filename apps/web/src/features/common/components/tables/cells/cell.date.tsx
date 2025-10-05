07c34eafa7b9edeea26240f53bee11101df6ca70
Carlo Nicora <carlo.nicora@gmail.com>
2025-10-05 23:47:59 +0200

    feat: add highlightExpired

--- apps/web/src/features/common/components/tables/cells/cell.date.tsx
+++ apps/web/src/features/common/components/tables/cells/cell.date.tsx
@@ -1,12 +1,13 @@
+import { cn } from "@/lib/utils";
 import { ColumnDef } from "@tanstack/react-table";
 
-export const cellDate = (params: { name: string; title: string }): ColumnDef<any> => {
+export const cellDate = (params: { name: string; title: string; highlightExpired?: boolean }): ColumnDef<any> => {
   return {
     id: params.name,
     accessorKey: params.name,
     header: params.title,
     cell: ({ row }) => (
-      <span className="text-muted-foreground text-xs">
+      <span className={cn(`text-muted-foreground text-xs`, params.highlightExpired ? `text-destructive` : ``)}>
         {row.getValue<Date>(params.name)
           ? row.getValue<Date>(params.name).toLocaleDateString("it", { dateStyle: "medium" })
           : ""}
